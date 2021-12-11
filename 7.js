315427ca7f22948489a634bd6801aed75052d940
Lukas Kurcik <lukurcik@microsoft.com>
2021-12-11 09:28:14 +0100

    day 7 part 2

--- 7.js
+++ 7.js
@@ -2,19 +2,27 @@ var fs = require('fs');
 fs.readFile('input7.txt', 'utf8', function(err, data) {
     if (err) throw err;
     const crabs = data.trim().split(",").map(x => parseInt(x));
-    console.log("1:", findOptimalFuelUsage(crabs));
+    console.log("1:", findOptimalFuelUsage(crabs, findFuelUsage1));
+    console.log("2:", findOptimalFuelUsage(crabs, findFuelUsage2));
 });
 
-function findOptimalFuelUsage(crabs) {
+function findOptimalFuelUsage(crabs, fuelUsageFn) {
     const minPos = Math.min(...crabs);
     const maxPos = Math.max(...crabs);
     let optimal = Number.MAX_VALUE;
     for (let i = minPos; i <= maxPos; i++) {
-        optimal = Math.min(optimal, findFuelUsage(crabs, i));
+        optimal = Math.min(optimal, fuelUsageFn(crabs, i));
     }
     return optimal;
 }
 
-function findFuelUsage(crabs, position) {
+function findFuelUsage1(crabs, position) {
     return crabs.map(crab => Math.abs(position - crab)).reduce((a,b) => a + b);
+}
+
+function findFuelUsage2(crabs, position) {
+    return crabs.map(crab => {
+        const n = Math.abs(position - crab);
+        return n * (n + 1) / 2;
+    }).reduce((a,b) => a + b);
 }
