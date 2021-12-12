0d2f0bd9f218cf8a8844c9d58663e99721abad69
Lukáš Kurčík <lukas.kurcik@gmail.com>
2021-12-12 09:47:16 +0100

    day 12 part b

--- 12.js
+++ 12.js
@@ -6,6 +6,7 @@ fs.readFile('input12.txt', 'utf8', function (err, data) {
     const edges = data.trim().replace(/\r/g, "").split("\n").map(x => x.split('-'));
     const graph = buildSimpleGraph(edges);
     console.log("1:", solve1(graph));
+    console.log("2:", solve2(graph));
 });
 
 function buildSimpleGraph(edges) {
@@ -19,9 +20,12 @@ function buildSimpleGraph(edges) {
         if (!graph[end]) {
             graph[end] = [];
         }
-        graph[start].push(end);
-        graph[end].push(start);
+        if (end !== 'start' && start !== 'end')
+            graph[start].push(end);
+        if (start !== 'start' && end !== 'end')
+            graph[end].push(start);
     }
+    console.log(graph);
     return graph;
 }
 
@@ -36,6 +40,31 @@ function solve1(graph, node = 'start', visited = new Set(['start'])) {
         visited.add(newNode);
         paths += solve1(graph, newNode, visited);
         visited.delete(newNode);
+    }
+    return paths;
+}
+
+function solve2(graph, node = 'start', visited = new Set(['start']), double = undefined, path = []) {
+    if (node === 'end') {
+        return 1;
+    }
+    let paths = 0;
+    for (const newNode of graph[node]) {
+        const isLowerCase = newNode === newNode.toLowerCase();
+        if (isLowerCase && visited.has(newNode)) {
+            if (double !== undefined) {
+                continue;
+            }
+            path.push(newNode);
+            paths += solve2(graph, newNode, visited, newNode, path);
+            path.pop();
+        } else {
+            visited.add(newNode);
+            path.push(newNode);
+            paths += solve2(graph, newNode, visited, double, path);
+            path.pop();
+            visited.delete(newNode);
+        }
     }
     return paths;
 }
