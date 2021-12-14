77b4cd8a329a2d04c2d2a8cddb23cba10ef752d3
Lukáš Kurčík <lukas.kurcik@gmail.com>
2021-12-14 13:16:46 +0100

    14 part2

--- 14.js
+++ 14.js
@@ -6,7 +6,7 @@ fs.readFile('input14.txt', 'utf8', function (err, data) {
     const input = data.trim().replace(/\r/g, "").split("\n");
     const [template, rules, letters] = prepareInupt(input);
     console.log("1:", solve(template, rules, letters, 10));
-    // console.log("2:", solve(template, rules, letters, 40));
+    console.log("2:", solve(template, rules, letters, 40));
 });
 
 function prepareInupt(input) {
@@ -21,11 +21,7 @@ function prepareInupt(input) {
 }
 
 function solve(template, rules, letters, rounds) {
-    const polymer = polymerizeIterative(template, rules, rounds).split('');
-    const counts = [];
-    for (const letter of letters) {
-        counts.push(polymer.filter(x => x === letter).length);
-    }
+    const counts = getCounts(template, rules, letters, rounds);
     return Math.max(...counts) - Math.min(...counts);
 }
 
@@ -33,14 +29,70 @@ function polymerizeIterative(template, rules, rounds) {
     let polymer = template;
     for (let round = 0; round < rounds; round++) {
         let newPolymer = polymer[0];
-        for (let i = 0; i < polymer.length - 1; i++) {
-            const base = polymer.substring(i, i + 2);
+        getParts(polymer).forEach((base) => {
             if (rules[base]) {
                 newPolymer += rules[base];
             }
             newPolymer += base[1];
-        }
+        });
         polymer = newPolymer;
     }
     return polymer;
+}
+
+
+function countLetters(polymer, letters) {
+    const counts = [];
+    const polymerSplit = polymer.split("");
+    for (const letter of letters) {
+        counts.push(polymerSplit.filter(x => x === letter).length);
+    }
+    return counts;
+}
+
+function getParts(polymer) {
+    const parts = [];
+    for (let i = 0; i < polymer.length - 1; i++) {
+        parts.push(polymer.substring(i, i + 2));
+    }
+    return parts;
+}
+
+function sumArrs(a, b) {
+    const sum = [];
+    for (let i = 0; i < a.length; i++) {
+        sum[i] = a[i] + b[i];
+    }
+    return sum;
+}
+
+// const memory = new Map();
+function getCounts(polymer, rules, letters, rounds, memory = new Map()) {
+    if (rounds <= 0 || polymer.length <= 1) {
+        return countLetters(polymer, letters);
+    }
+    const key = `${polymer}_${rounds}`;
+    if (!memory.has(key)) {
+        if (polymer.length == 2) {
+            const newPolymer = polymerizeIterative(polymer, rules, 1);
+            let counts = countLetters(newPolymer, letters);
+            getParts(newPolymer).forEach(part => {
+                const partCounts = getCounts(part, rules, letters, rounds - 1, memory);
+                partCounts[letters.indexOf(part[0])] -= 1;
+                partCounts[letters.indexOf(part[1])] -= 1;
+                counts = sumArrs(counts, partCounts);
+            });
+            memory.set(key, counts.join(","));
+        } else {
+            let counts = countLetters(polymer, letters);
+            getParts(polymer).forEach(part => {
+                const partCounts = getCounts(part, rules, letters, rounds, memory);
+                partCounts[letters.indexOf(part[0])] -= 1;
+                partCounts[letters.indexOf(part[1])] -= 1;
+                counts = sumArrs(counts, partCounts);
+            });
+            memory.set(key, counts.join(","));
+        }
+    }
+    return memory.get(key).split(",").map(x => parseInt(x));
 }
